7fec64d1ae1b44e7c0553cd7d20f952cf319d683
704692689@qq.com <704692689@qq.com>
2016-09-18 11:28:04 +0800

    测试

---
+++ edward-skillful-web/src/test/java/com/test/Test04.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by weideliang on 2016/9/18.
+ */
+public class Test04 {
+    public static void main(String[] args) {
+        System.out.println("完美世界");
+    }
+}
