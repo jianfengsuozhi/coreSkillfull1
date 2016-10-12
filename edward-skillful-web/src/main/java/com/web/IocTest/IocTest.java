ba9c814a32fe8d13028d309d2f355bc4cdefcdf9
704692689@qq.com <704692689@qq.com>
2016-10-12 14:40:05 +0800

    spring ioc

--- edward-skillful-web/src/main/java/com/web/IocTest/IocTest.java
+++
@@ -1,7 +0,0 @@
-package com.web.IocTest;
-
-/**
- * Created by weideliang on 2016/10/11.
- */
-public class IocTest {
-}
--- edward-skillful-web/src/main/java/com/web/test/TestIocImpl.java
+++
@@ -1,7 +0,0 @@
-package com.web.test;
-
-/**
- * Created by weideliang on 2016/10/11.
- */
-public class TestIocImpl {
-}
