16eac2cac7e8a8ca071a4bdbc0805cfe535a28f2
704692689@qq.com <704692689@qq.com>
2016-10-25 15:35:07 +0800

    数据库 修改

--- edward-skillful-web/src/main/java/com/web/util/SearchParamHandlerMethodArgumentResolver.java
+++ edward-skillful-web/src/main/java/com/web/util/SearchParamHandlerMethodArgumentResolver.java
@@ -1,5 +1,9 @@
 package com.web.util;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.util.FieldUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -9,13 +13,14 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.lang.reflect.Field;
-import java.lang.reflect.Type;
+import java.lang.reflect.*;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 保存搜索条件
@@ -63,7 +68,7 @@ public class SearchParamHandlerMethodArgumentResolver implements HandlerMethodAr
     }
 
 
-    public void fillCondition(Object condition,Map<String, String[]> paramMap) throws IllegalAccessException {
+    public void fillCondition(Object condition,Map<String, String[]> paramMap) throws IllegalAccessException, InvocationTargetException, ParseException {
         for (Map.Entry<String,String[]> param : paramMap.entrySet()) {
             Field field = null;
             try {
@@ -76,11 +81,121 @@ public class SearchParamHandlerMethodArgumentResolver implements HandlerMethodAr
             if(null == param.getValue()){//当传过来的值为null时session的值不变
                 continue;
             }
-            //普通类型
-            this.setValue(field,condition,param.getValue());
+
+            if(field.getType().isArray()) {//生成数组
+                this.setArrayValue(condition, field, param.getValue());
+            }else if (List.class.equals(field.getType()) ||
+                        ArrayUtils.contains(field.getType().getInterfaces(), List.class))// 生成LIST
+            {
+                this.parseList(field, condition, param.getKey(), param.getValue(),
+                        this.getActualTypeFieldType(field));
+            } else if (Set.class.equals(field.getType()) ||
+                    ArrayUtils.contains(field.getType().getInterfaces(), Set.class))// 生成SET
+            {
+                this.parseSet(field, condition, param.getKey(), param.getValue(),
+                        this.getActualTypeFieldType(field));
+            } else{
+                //普通类型
+                this.setValue(field,condition,param.getValue());
+            }
+
         }
     }
 
+    /**
+     * LIST,SET得到泛型类型，如果没有指定泛型或有多个泛型存在，返回NULL
+     *
+     * @param field
+     * @return
+     * @author liufei
+     * @date 2015年8月12日 下午8:44:28
+     */
+    private Type getActualTypeFieldType(Field field) {
+
+        if (field.getGenericType() instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+            Type[] types = parameterizedType.getActualTypeArguments();
+            if (types.length == 1)
+                return parameterizedType.getActualTypeArguments()[0];
+            else
+                return null;
+        } else
+            return null;
+    }
+
+    /**
+     * 转换为list
+     * @param field
+     * @param condition
+     * @param fieldName
+     * @param values
+     * @param elType
+     * @throws ParseException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    private void parseList(Field field, Object condition, String fieldName,
+                           String[] values, Type elType) throws ParseException, IllegalAccessException, InvocationTargetException {
+
+        List list = Lists.newArrayList();
+        for (String value : values) {
+            if (value == null)
+                continue;
+            list.add(this.valueTypeConverter(elType, value));
+        }
+        BeanUtils.setProperty(condition, fieldName, list);
+    }
+
+    public void setArrayValue(Object condition,Field field,String[] values) throws IllegalAccessException {
+        Class<?> componentType = field.getType().getComponentType();
+        int length = values.length;
+        Object array = Array.newInstance(componentType, length);
+        for(int i=0; i<length; i++){
+            if(null == values[i]){
+                continue;
+            }
+            //转换类型
+            Object value = this.valueTypeConverter(componentType, values[i]);
+            Array.set(array,i,value);
+        }
+        //将数组值赋给该对象
+        setPropertyValue(field,condition,array);
+    }
+
+    /**
+     * 放入SET对象值
+     *
+     * @param field     ;
+     * @param condition
+     * @param fieldName
+     * @param values
+     * @param elType
+     * @throws ParseException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     * @author liufei
+     * @date 2015年8月12日 下午8:46:15
+     */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private void parseSet(Field field, Object condition, String fieldName,
+                          String[] values, Type elType) throws ParseException, IllegalAccessException, InvocationTargetException {
+
+        Set set = Sets.newHashSet();
+        for (String value : values) {
+            if (value == null)
+                continue;
+            set.add(this.valueTypeConverter(elType, value));
+        }
+        BeanUtils.setProperty(condition, fieldName, set);
+    }
+
+    /**
+     * 设置需要的类型的值
+     * @param field
+     * @param condition
+     * @param value
+     * @throws IllegalAccessException
+     */
     private void setValue(Field field, Object condition, String[] value) throws IllegalAccessException {
         if(value.length == 1 && value[0] != null){
             //1 得到值 主要是类型转换
